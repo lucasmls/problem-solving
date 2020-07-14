57b2a3b5cc39db8686f263484e84a295925b3a5c
Lucas Mendes <lucaslsbh1@gmail.com>
2020-07-14 07:09:26 -0300

    :sparkles: add fizzbuzz solution

---
+++ interview-algorithms-course/fizzbuzz/index.js
@@ -0,0 +1,39 @@
+/**
+ * Write a program that console logs the numbers
+ * from 1 to n. But for multiples of three print
+ * “fizz” instead of the number and for the multiples
+ * of five print “buzz”. For numbers which are multiples
+ * of both three and five print “fizzbuzz”.
+
+ *   fizzBuzz(5);
+ *   1
+ *   2
+ *   fizz
+ *   4
+ *   buzz
+*/
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz")
+      continue
+    }
+
+    if (i % 3 === 0) {
+      console.log("fizz")
+      continue
+    }
+
+    if (i % 5 === 0) {
+      console.log("buzz")
+      continue
+    }
+
+    console.log(i)
+  }
+}
+
+// fizzBuzz(15);
+
+module.exports = fizzBuzz;
