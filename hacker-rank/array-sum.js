b2b3b0909224cb42ccc9def3b53da141be733820
Lucas Mendes <lucaslsbh1@gmail.com>
2020-08-12 07:21:31 -0300

    :building_construction: converting a simple array sum to go

--- hacker-rank/array-sum.js
+++
@@ -1,6 +0,0 @@
-function simpleArraySum(ar) {
-  return ar.reduce((acc, item) => acc += item)
-}
-
-const result = simpleArraySum([1, 2, 3, 4, 10, 11])
-console.log(result)
---
+++ simplearraysum/simplearraysum_test.go
@@ -0,0 +1,23 @@
+package simplearraysum
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	testCases := []struct {
+		input  []int
+		output int
+	}{
+		{
+			input:  []int{5, 10, 15},
+			output: 30,
+		},
+	}
+	for _, tc := range testCases {
+		result := SimpleArraySum(tc.input)
+		assert.Equal(t, tc.output, result)
+	}
+}
