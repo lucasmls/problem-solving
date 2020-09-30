d8a31e69b872d3c350ea30f230c0b93ee01101de
Lucas Mendes <lucaslsbh1@gmail.com>
2020-09-30 07:14:55 -0300

    :recycle: add another solution for product problem

--- productofallnumbers/productofallnumbers.go
+++ productofallnumbers/productofallnumbers.go
@@ -10,19 +10,77 @@ package productofallnumbers
  * Follow-up: what if you can't use division?
  */
 
-// ProductNumbers ...
-func ProductNumbers(list []int32) []int32 {
-	products := []int32{}
-
-	var listSum int32 = 1
-	for i := 0; i < len(list); i++ {
-		listSum *= list[i]
+func Reverse(list []int32) []int32 {
+	result := []int32{}
+	for i := len(list) - 1; i >= 0; i-- {
+		result = append(result, list[i])
 	}
 
+	return result
+}
+
+// ProductNumbers ...
+func ProductNumbers(list []int32) []int32 {
+	prefix := []int32{}
 	for i := 0; i < len(list); i++ {
-		result := listSum / list[i]
-		products = append(products, result)
+		num := list[i]
+
+		if len(prefix) != 0 {
+			prefix = append(prefix, prefix[len(prefix)-1]*num)
+			continue
+		}
+
+		prefix = append(prefix, num)
+	}
+
+	reversedList := Reverse(list)
+
+	suffix := []int32{}
+	for i := 0; i < len(list); i++ {
+		num := reversedList[i]
+
+		if len(suffix) != 0 {
+			suffix = append(suffix, suffix[len(suffix)-1]*num)
+			continue
+		}
+
+		suffix = append(suffix, num)
+	}
+
+	suffix = Reverse(suffix)
+
+	products := []int32{}
+	for i := 0; i < len(list); i++ {
+		if i == 0 {
+			products = append(products, suffix[i+1])
+			continue
+		}
+
+		if i == len(list)-1 {
+			products = append(products, prefix[i-1])
+			continue
+		}
+
+		product := prefix[i-1] * suffix[i+1]
+		products = append(products, product)
 	}
 
 	return products
 }
+
+// ProductNumbers ...
+// func ProductNumbers(list []int32) []int32 {
+// 	products := []int32{}
+
+// 	var listSum int32 = 1
+// 	for i := 0; i < len(list); i++ {
+// 		listSum *= list[i]
+// 	}
+
+// 	for i := 0; i < len(list); i++ {
+// 		result := listSum / list[i]
+// 		products = append(products, result)
+// 	}
+
+// 	return products
+// }
