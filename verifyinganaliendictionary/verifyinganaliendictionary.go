4056a38ec392663f3239f5557181820c6fd78a1e
Lucas Mendes <lucaslsbh1@gmail.com>
2020-12-07 07:35:53 -0300

    :pencil: add problem description for verifying an alien dictionary

--- verifyinganaliendictionary/verifyinganaliendictionary.go
+++ verifyinganaliendictionary/verifyinganaliendictionary.go
@@ -4,6 +4,23 @@ import (
 	"math"
 )
 
+/**
+ * In an alien language, surprisingly they also use english lowercase letters, but possibly
+ * in a different order. The order of the alphabet is some permutation of lowercase letters.
+ * Given a sequence of words written in the alien language, and the order of the alphabet,
+ * return true if and only if the given words are sorted lexicographicaly in this alien language.
+ *
+ * Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+ * Output: true
+ * Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+ *
+ * Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+ * Output: false
+ * Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+ *
+ * @link https://leetcode.com/problems/verifying-an-alien-dictionary/
+ */
+
 func isAlienSorted(words []string, order string) bool {
 	orderMap := map[string]int{}
 	for i, r := range order {
